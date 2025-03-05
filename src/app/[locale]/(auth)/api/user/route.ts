d7fe0e7f3dd14cb86e76ce908b199f98d709eede
Quân Ninh <ninhquan391@gmail.com>
2025-03-05 21:31:42 +0700

    update crud

--- src/app/[locale]/(auth)/api/user/route.ts
+++ src/app/[locale]/(auth)/api/user/route.ts
@@ -1,7 +1,18 @@
 import type { NextRequest } from 'next/server';
-
-import { getUsers } from './userServices';
+import { createUser, deleteUser, getUsers, updateUser } from './userServices';
 
 export const GET = async (request: NextRequest) => {
   return getUsers(request);
+};
+
+export const POST = async (request: NextRequest) => {
+  return createUser(request);
+};
+
+export const PUT = async (request: NextRequest) => {
+  return updateUser(request);
+};
+
+export const DELETE = async (request: NextRequest) => {
+  return deleteUser(request);
 };
